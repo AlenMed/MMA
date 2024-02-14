00fa22965b9c304342359eb05118a1d39ffe4572
AlenMed <alenmedakovic@gmail.com>
2024-02-14 18:13:46 +0100

    initial commit

---
+++ MMA/MMAApp.swift
@@ -0,0 +1,32 @@
+//
+//  MMAApp.swift
+//  MMA
+//
+//  Created by ektamannen on 14/02/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MMAApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
